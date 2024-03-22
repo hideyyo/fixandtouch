fe5adfed0a968e69eb679f12c2174d0d67eb59d6
hideyyo <98717943+hideyyo@users.noreply.github.com>
2024-03-22 06:05:53 -0500

    v1.1
    Better error handling, fixing some bugs in fixcsv logic, better outputting of errors.

--- cli-ul-fixandtouch.py
+++ cli-ul-fixandtouch.py
@@ -8,22 +8,28 @@ p = parser.parse_args()
 
 
 def fixcsv(df):
-    df2 = df[~((df['Address1'].str.len() > 25) | (df['Address2'].str.len() > 1))].copy(True)
-    df2 = df2.fillna('')
-    
-    df2.to_csv(inputstr, index = False)
-    print(f'{inputstr} fixed sucessfully!')
+    try:
+        df2 = df[~((df['Address1'].str.len() > 25) or df(['City'].str.len() >= 20) or (df['Address2'].str.len() > 1))].copy(True)
+        df2 = df2.fillna('')
+        df2.to_csv(inputstr, index = False)
+        print(f'{inputstr} fixed sucessfully!')
+    except:
+        print(f'File {inputstr} already fixed!')
     
 def touchcsv():
-    df2 = df[(df['Address1'].str.len() > 25) | (df['Address2'].str.len() > 1)].copy(True)
-    df2.to_csv('longlabels.csv', index = False)
-    print('File touched up successfully!')
+    try:
+        df2 = df[(df['Address1'].str.len() > 25) | (df['Address2'].str.len() > 1)].copy(True)
+        df2.to_csv('longlabels.csv', index = False)
+        print('File touched up successfully!')
+    except:
+        print(f'File {inputstr} doesn\'t need to be touched!')
             
         
 
 exit = False
 
-inputstr = p.filepath
+extention_name = '.csv'
+inputstr = p.filepath.stem + extention_name
 while(exit == False):
     try:
         df = pd.read_csv(inputstr)         
@@ -31,5 +37,5 @@ while(exit == False):
         touchcsv()
         exit = True
     except:
-        print(f'Unable to find file {inputstr} or already fixed. Please check for typos and try again.')
+        print(f'File {inputstr} not found, please check for typos and try again.')
         exit = True
